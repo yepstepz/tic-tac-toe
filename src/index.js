6c4ba002160cb4e0011ed22ea49770a1fb2c2230
UDemis <support@demis.ru>
2017-05-05 17:22:24 +0300

    class to func

--- src/index.js
+++ src/index.js
@@ -2,20 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-  	let value = this.props.value;
+function Square(props) {
     return (
-      <button className="square" onClick = { () => alert(value) }>
-        {value}
+      <button className="square" onClick={props.onClick }>
+      {props.value}
       </button>
     );
-  }
 }
 
 class Board extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+       squares: Array(9).fill(null)
+    };
+  }
   renderSquare(i) {
-    return <Square  value={i} />;
+    return <Square 
+    		value={this.state.squares[i]} 
+    		onClick={ () => this.handleClick(i) }
+    		/>;
+  }
+  handleClick(i){
+  	const squares = this.state.squares.slice();
+  	squares[i] = "X";
+  	this.setState({
+  		squares : squares
+  	})
   }
 
   render() {
